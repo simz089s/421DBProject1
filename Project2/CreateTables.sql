f0d377325ed1228d46caf327b4a5fd4554c13775
Elvric <elvric.trombert@mail.mcgill.ca>
2018-02-23 11:39:36 -0500

    updated the tables

--- Project2/CreateTables.sql
+++ Project2/CreateTables.sql
@@ -1,24 +1,43 @@
-CREATE TYPE Gen AS ENUM ('male','female','other');
+CREATE TYPE Gen AS ENUM ('MALE','FEMALE','OTHER');
+CREATE 
+	TABLE
+		Clients
+		(
+			cID integer PRIMARY KEY,
+			email varchar(30) NOT NULL,
+			phone varchar(20) NOT NULL,
+			address text NOT NULL,
+			account varchar(30) NOT NULL
+		);
 CREATE
 	TABLE
-		Clients 
+		Individuals 
 		(
 		cID integer PRIMARY KEY,
 		fName VARCHAR(30) NOT NULL,
-		lNmame varchar(30) NOT NULL,
-		email varchar(30) NOT NULL,
-		phone varchar(20) NOT NULL,
+		lName varchar(30) NOT NULL,
 		gender Gen NOT NULL,
-		address text NOT NULL,
 		birthdate date NOT NULL,
-		account varchar(30) NOT NULL UNIQUE 
+		FOREIGN KEY (cID) REFERENCES Clients
 		);
 CREATE 
 	TABLE 
-		InsureancePlans
+		InsurancePlans
 		(
 			planID integer PRIMARY KEY,
 			coverage text NOT NULL,
 			price integer NOT NULL,
 			pName varchar(30) NOT NULL
 		);
+CREATE
+	TABLE
+		Companies
+		(
+			cID integer PRIMARY KEY,
+			compName varchar(30) NOT NULL,
+			numEmploy integer NOT NULL,
+			FOREIGN KEY (cID) REFERENCES Clients
+		);
+
+
+		
