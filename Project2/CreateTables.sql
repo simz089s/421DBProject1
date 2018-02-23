d38e5499e6d620610c6f7c00ca30ed056be1954a
Elvric <elvric.trombert@mail.mcgill.ca>
2018-02-23 11:15:32 -0500

    updated data type

--- Project2/CreateTables.sql
+++ Project2/CreateTables.sql
@@ -3,14 +3,14 @@ CREATE
 	TABLE
 		Clients 
 		(
-		cID INTEGER PRIMARY KEY,
+		cID integer PRIMARY KEY,
 		fName VARCHAR(30) NOT NULL,
 		lNmame varchar(30) NOT NULL,
 		email varchar(30) NOT NULL,
-		phone integer NOT NULL,
+		phone varchar(20) NOT NULL,
 		gender Gen NOT NULL,
 		address text NOT NULL,
-		birthdate int NOT NULL,
-		account int NOT NULL UNIQUE 
+		birthdate date NOT NULL,
+		account varchar(30) NOT NULL UNIQUE 
 		);
-
+DROP TABLE Clients;
