bf65fb039d16812f0898a347a55bd3ba4e7ea7bb
Simon Zheng <simz97@outlook.com>
2018-02-28 20:56:25 -0500

    Q8 Done

--- Project2/TOSUBMIT/q8.sql
+++ Project2/TOSUBMIT/q8.sql
@@ -1,51 +1,33 @@
--- Project the name of every table that contains a column named ''
-SELECT
-	table_name
-FROM
-	information_schema.columns
-WHERE
-	column_name = 'address';
-
--- Project the sorted number of used rows of all the tables in our schema (cs421g24)
--- Can be useful to check which empty tables are left and how many rows each have
-SELECT
-	schemaname,
-	relname,
-	n_live_tup
-FROM
-	pg_stat_user_tables
-WHERE
-	schemaname = 'cs421g24'
-ORDER BY
-	n_live_tup,
-	relname;
-
--- Just some normal projection
-SELECT
-	*
-FROM
-	subscriptions
-WHERE
-	startdate > enddate
---ORDER BY
-	;
-
--- Delete table rows
--- Please be careful and passively sanitize (neutralize) these kind of SQL statements when you aren't using them
-DELETE
-FROM
-	
-WHERE
-	id < 0;
-
--- Drop single table
--- Once again be careful and neutralize the SQL statement when done...
-DROP
-	TABLE
-		;
-
--- Table altering (also careful)
+/* Alter table again for Q8 */
 ALTER TABLE
-	cs421g24.prescriptions ALTER COLUMN did TYPE INT8
-		USING did::INT8;
+	individuals ADD /*CONSTRAINT individuals_cid_check*/ CHECK(
+		cid > 0
+	);
+
+INSERT
+	INTO
+		individuals
+	VALUES(
+		0,
+		'a',
+		'b',
+		'2000-01-01',
+		'Other'
+	);
+
+/*-------------------------------------------------------------------*/
+ALTER TABLE
+	companies ADD /*CONSTRAINT companies_cid_check*/
+	CHECK(
+		companies.numemploy > 0
+	);
+
+INSERT
+	INTO
+		companies
+	VALUES(
+		51,
+		'Inc. Co.',
+		-1
+	);
 
