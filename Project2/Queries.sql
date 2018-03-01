872fee01668f17ca92c8fb9540518f8af38cc8be
Simon Zheng <simz97@outlook.com>
2018-02-28 19:46:32 -0500

    Fix Queries.sql

--- Project2/Queries.sql
+++ Project2/Queries.sql
@@ -1,15 +1,7 @@
-INSERT
-	INTO
-		clients
-	VALUES(
-		2,
-		'j.dupond@mail.com',
-		'4389352104',
-		'apt 5 rue des fleurs H16 2l8 Montreal',
-		'100-030-121213'
-	);
+-- Question 5
+ SELECT cid
+FROM clients
+EXCEPT SELECT cid
+FROM subscriptions S
+WHERE S.enddate > '2018-01-01';
 
-SELECT * FROM clients
-
-/* Question 5 */
-SELECT cid FROM clients EXCEPT SELECT cid FROM subscriptions S WHERE S.enddate > '2018-01-01';  
