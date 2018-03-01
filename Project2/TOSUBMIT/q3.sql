2352ae4ac5e775ab951145f1be178a0da31b5a28
Su <su.yu@mail.mcgill.ca>
2018-03-01 15:24:08 -0500

    five insert commands

---
+++ Project2/TOSUBMIT/q3.sql
@@ -0,0 +1,5 @@
+insert into DRUGS (duid, dname, manufacturer, price) values (101, 'Ready Foundation', 'Bare Escentuals Beauty Inc.', '$14.24');
+insert into DRUGS (duid, dname, manufacturer, price) values (102, 'Ibuprofen', 'State of Florida DOH Central Pharmacy', '$51.49');
+insert into DRUGS (duid, dname, manufacturer, price) values (103, 'Escitalopram', 'Legacy Pharmaceutical Packaging', '$81.97');
+insert into DRUGS (duid, dname, manufacturer, price) values (104, 'Lisinopril and hydrochlorothiazide', 'Watson Laboratories, Inc.', '$60.23');
+insert into DRUGS (duid, dname, manufacturer, price) values (105, 'MEDI-FIRST Non-Aspirin', 'Unifirst First Aid Corporation', '$42.44');
