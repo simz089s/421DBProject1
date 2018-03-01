80b07b4c2f7d8216cff8d65462035a6d65e32f41
Simon Zheng <simz97@outlook.com>
2018-02-28 20:20:01 -0500

    Q5 Female client prescription

--- Project2/Queries.sql
+++ Project2/Queries.sql
@@ -1,7 +1,19 @@
--- Question 5
- SELECT cid
+/* Question 5 */
+
+/*  */
+SELECT cid
 FROM clients
 EXCEPT SELECT cid
 FROM subscriptions S
 WHERE S.enddate > '2018-01-01';
+
+/* Project pid, cid, and name of female clients who have prescriptions, ordered by pid */
+SELECT pid, I.cid, fname, lname
+FROM prescriptions P, individuals I
+WHERE P.cid = I.cid AND gender = 'Female'
+ORDER BY pid;
+
+--IN( SELECT I.cid
+--FROM individuals I
+--WHERE I.gender = 'Female' )
 
