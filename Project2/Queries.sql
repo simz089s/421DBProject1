022e2c0dd8affee5fed2e5ac8c629b335075c2db
Simon Zheng <simz97@outlook.com>
2018-03-01 12:26:15 -0500

    Queries update

--- Project2/Queries.sql
+++ Project2/Queries.sql
@@ -11,6 +11,7 @@ WHERE S.enddate > '2018-01-01';
 SELECT pid, I.cid, fname, lname
 FROM prescriptions P, individuals I
 WHERE P.cid = I.cid AND gender = 'Female'
+--GROUP BY gender
 ORDER BY pid;
 
 /*Shows the amount of money spent by both sexes*/
@@ -19,18 +20,39 @@ FROM receipts R, prescriptions P, individuals I
 WHERE R.pid = P.Pid AND P.cid = I.cid 
 GROUP BY I.gender;
 
-/*Shows the average spent on plans by clients based on gender*/
+/*Shows the average money spent on plans by clients based on gender*/
 SELECT I.gender,AVG(P.price::NUMERIC) 
 FROM subscriptions S, individuals I, insuranceplans P
 WHERE S.planid = P.planid AND I.cid = S.cid
 GROUP BY I.gender;
 
-SELECT I.birthdate
-FROM 	insuranceclaims IC,	receipts R, pharmacists Ph, prescriptions Pr, clients C, individuals I
-WHERE IC.rid = R.rid AND Ph.did = R.did
-	AND Pr.pid = R.pid AND C.cid = Pr.cid AND C.cid IN(SELECT I.cid FROM individuals WHERE I.birthdate > '1971-01-01')
-GROUP BY I.birthdate
-ORDER BY I.birthdate;
+/*  */
+SELECT
+	I.birthdate
+FROM
+	insuranceclaims IC,
+	receipts R,
+	pharmacists Ph,
+	prescriptions Pr,
+	clients C,
+	individuals I
+WHERE
+	IC.rid = R.rid
+	AND Ph.did = R.did
+	AND Pr.pid = R.pid
+	AND C.cid = Pr.cid
+	AND C.cid IN(
+		SELECT
+			I.cid
+		FROM
+			individuals
+		WHERE
+			I.birthdate > '1971-01-01'
+	);
+GROUP BY
+	I.birthdate
+ORDER BY
+	I.birthdate;
 --IN( SELECT I.cid
 --FROM individuals I
 --WHERE I.gender = 'Female' )
