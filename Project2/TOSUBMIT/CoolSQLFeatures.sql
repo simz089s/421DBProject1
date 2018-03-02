8e670753cc3c3b55640e5c5d891fa968ad6f1b12
Su YU <syu47@ds2.CS.McGill.CA>
2018-03-01 20:03:04 -0500

    moved files

--- Project2/TOSUBMIT/CoolSQLFeatures.sql
+++
@@ -1,3 +0,0 @@
-SELECT DISTINCT Rv.cid,AVG(R.totalprice::NUMERIC) OVER (PARTITION BY I.cid)
-FROM receiptclientview Rv, individuals I,receipts R WHERE 
-I.cid = Rv.cid AND age(current_date,I.birthdate)>'50 years'; 
