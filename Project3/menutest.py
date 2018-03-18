00fa64a5d99eff03548c2162ddf305c176a94382
Simon Zheng <simz97@outlook.com>
2018-03-18 17:35:55 -0400

    menutest2

--- Project3/menutest.py
+++ Project3/menutest.py
@@ -12,9 +12,15 @@ import psycopg2
 from tkinter import *
 import getpass
 
-def someEvent(event):
-    w = event.widget
+root = Tk()
+action = IntVar()
+frame = Frame(root, bg='pink', width=500, height=500)
+class StartPage(Frame):
+    def __init__(self, parent, controller):
+        Label(self,text="start page",font=LARGE_FONT).pacK()
 
+    global root
+    root.destroy()
 def main(argc, args):
     ##################################################
     # FOR TESTING ONLY REMOVE HARDCODED PASSWORD AFTER
@@ -23,11 +29,21 @@ def main(argc, args):
     ##################################################
     conn = psycopg2.connect(dbname='cs421', user='cs421g24', password=pswd, host='comp421.cs.mcgill.ca')
     cursor = conn.cursor()
-    root = Tk()
-    frame = Frame(root, bg='pink', width=500, height=500)
-    frame.pack()
-    option1 = Radiobutton(root, text='option1',value=1)
-    option1.pack()
+    frame.grid()
+    quitbutton = Button(frame,text="Quit",command=quit)
+    quitbutton.grid()
+    global action
+    option1 = Radiobutton(frame, text='option1',value=1,variable=action)
+    option2 = Radiobutton(frame, text='option2',value=2,variable=action)
+    option3= Radiobutton(frame, text='option3',value=3,variable=action)
+    option4 = Radiobutton(frame, text='option4',value=4,variable=action)
+    option5= Radiobutton(frame, text='option5',value=5,variable=action)
+    oparr = [option1,option2,option3,option4,option5]
+    option1.select()
+    selection = Button(frame,text='Select',command=selection_action);
+    for op in oparr:
+        op.grid()
+    selection.grid()
     root.mainloop()
     return 0
 
