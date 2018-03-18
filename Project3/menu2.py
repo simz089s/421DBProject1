b490296f38b6024bd52f29dd9e4cd54e3fbff373
Elvric <elvric.trombert@mail.mcgill.ca>
2018-03-18 19:23:33 -0400

    Did the first query

--- Project3/menu2.py
+++ Project3/menu2.py
@@ -57,7 +57,7 @@ class StartPage(tk.Frame):
         label.pack(pady=10,padx=10)
 
         option = tk.IntVar()
-        option1 = tk.Radiobutton(self,text='Option1',value=1,variable=option)
+        option1 = tk.Radiobutton(self,text='Add Client',value=1,variable=option)
         option1.pack()
         option2 = tk.Radiobutton(self,text='Option2',value=2,variable=option)
         option2.pack()
@@ -68,7 +68,7 @@ class StartPage(tk.Frame):
         option5 = tk.Radiobutton(self,text='Option5',value=5,variable=option)
         option5.pack()
 
-        select_bt = tk.Button(self,text="Next",command=lambda: controller.show_frame(option.get()))
+        select_bt = tk.Button(self,text="Select",command=lambda: controller.show_frame(option.get()))
         select_bt.pack()
         quit_bt = tk.Button(self,text="Quit",command=quit)
         quit_bt.pack()
@@ -79,8 +79,58 @@ class StartPage(tk.Frame):
 class Option1(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self,text="You choose option 1",font=LARGE_FONT)
+        label = tk.Label(self,text="Add Client",font=LARGE_FONT)
         label.pack(pady=10,padx=10)
+        email = tk.Label(self,text="e-mail")
+        email.pack()
+        self.e1 = tk.Entry(self)
+        self.e1.pack()
+        phone = tk.Label(self,text="phone")
+        phone.pack()
+        self.e2= tk.Entry(self)
+        self.e2.pack()
+        address = tk.Label(self,text="Address")
+        address.pack()
+        self.e3 = tk.Entry(self)
+        self.e3.pack()
+        account = tk.Label(self,text="account")
+        account.pack()
+        self.e4 = tk.Entry(self)
+        self.e4.pack()
+        submit_btn = tk.Button(self,text="Submit",command=self.addclient)
+        submit_btn.pack()
+        self.message = tk.Label(self,text='') 
+        goBack = tk.Button(self,text="<- Back",command=lambda: controller.show_frame(0))
+        goBack.pack()
+        goBack.pack()
+        quit_bt = tk.Button(self,text="Quit",command=quit)
+        self.message.pack()
+        quit_bt.pack()
+    def addclient(self):
+        global cursor
+        argtuple = (self.e1.get(),self.e2.get(),self.e3.get(),self.e4.get())
+        try:
+            for e in argtuple:
+                if e=='':
+                    raise Exception("All fields must have a value")
+            cursor.execute("INSERT INTO clients VALUES (%s,%s,%s,%s)",argtuple)
+            conn.commit()
+            self.message.config(text="successfully added %s" %(self.e1.get()))
+        except Exception as e:
+            self.message.config(text=str(e))
+        
+        finally:
+            self.e1.delete(0, tk.END)
+            self.e2.delete(0, tk.END)
+            self.e3.delete(0, tk.END)
+            self.e4.delete(0, tk.END)
+            self.update()
+
+
+        
+
+            
+
 
 class Option2(tk.Frame):
     def __init__(self, parent, controller):
@@ -111,10 +161,11 @@ APP = Insurance()
 
 def quit():
     APP.destroy()
+    conn.close()
 
 def main(argc, args):
     APP.mainloop()
-
+    conn.close()
 
 if __name__ == "__main__":
     sys.exit(main(len(sys.argv), sys.argv))
