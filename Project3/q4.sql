7618187cb6970f2614cfceb2501552f805b717c5
Abhijay Gupta <AbhijayGupta@wpa079181.wireless.mcgill.ca>
2018-03-23 19:31:48 -0400

    also getting months now which had 0 new subscriptions

--- Project3/q4.sql
+++ Project3/q4.sql
@@ -2,8 +2,9 @@
 
 /*
  * Query to extract number of new subscriptions per month
- * in the year 2018, sorted by month. Months which have
- * no new subscriptions are not included in the result.
+ * in the year 2018, sorted by month. First part of the query
+ * gets months which have new subscriptions and second part
+ * gets the month numbers which have 0 new subscriptions.
  * (Also, casting the month to int)
  */
 SELECT CAST(EXTRACT(MONTH FROM startdate) AS int) AS MONTH, count(subid) AS num_new_subscriptions
@@ -11,6 +12,17 @@ FROM subscriptions S
 WHERE
 	EXTRACT(YEAR FROM startdate) = 2018
 GROUP BY EXTRACT(MONTH FROM startdate)
-ORDER BY EXTRACT(MONTH FROM startdate)
-;
 
+UNION
+
+SELECT column1 AS MONTH, 0 AS num_new_subscriptions
+FROM (VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12)) months
+EXCEPT
+(SELECT CAST(EXTRACT(MONTH FROM startdate) AS int), 0 AS num_new_subscriptions
+FROM subscriptions S
+WHERE
+	EXTRACT(YEAR FROM startdate) = 2018
+	GROUP BY EXTRACT(MONTH FROM startdate)
+)
+ORDER BY MONTH
+;
